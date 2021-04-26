743d666265a1b92c497ba68db1e1ffbc74af2f93
James Wheadon <james.wheadon@yahoo.com>
2021-04-26 10:06:15 +0100

    move websockets to own file, seperate from api

---
+++ websocket.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+const port = process.env.PORT || 5001;
+
+server.listen(port, () => {
+  console.log('listening on *:5001');
+});
