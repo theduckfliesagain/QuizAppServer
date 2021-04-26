93330595c646d7573291be8f83606ea156013685
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-04-26 11:31:55 +0100

    add quiz controllers

---
+++ controllers/quizzes.js
@@ -0,0 +1,50 @@
+const Quiz = require('../models/Quiz');
+
+async function index (req, res) {
+    try {
+        const quizzes = await Quiz.all;
+        res.status(200).json(quizzes)
+    } catch (err) {
+        res.status(500).json({err})
+    }
+}
+
+async function show (req, res) {
+    try {
+        const quiz = await Quiz.findById(req.params.id);
+        res.status(200).json(quiz)
+    } catch (err) {
+        res.status(404).json({err})
+    }
+}
+
+async function create (req, res) {
+    try {
+        const quiz = await Quiz.create(req.body);
+        res.status(201).json(quiz)
+    } catch (err) {
+        res.status(422).json({err})
+    }
+}
+
+async function update (req, res) {
+    try {
+        let quiz = await Quiz.findById(req.params.id);
+        quiz = await quiz.update(req.body);
+        res.status(200).json(quiz)
+    } catch (err) {
+        res.status(422).json({err})
+    }
+}
+
+// async function destroy (req, res) {
+//     try {
+//         const quiz = await Quiz.findById(req.params.id);
+//         const resp = await quiz.destroy();
+//         res.status(204).end();
+//     } catch (err) {
+//         res.status(404).json({err});
+//     };
+// }
+
+module.exports = { index, show, create, update, destroy }
