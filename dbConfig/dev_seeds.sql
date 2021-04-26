4586e2825f1fe9eca7ba502742686b008c8cdcd8
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-04-26 11:32:11 +0100

    update dev seeds

--- dbConfig/dev_seeds.sql
+++ dbConfig/dev_seeds.sql
@@ -1,8 +1,8 @@
 INSERT INTO Users (name, highscore)
 VALUES ('duck', 5);
 
-INSERT INTO Quizzes (category)
-VALUES ('Entertainment');
+INSERT INTO Quizzes (category, difficulty, length)
+VALUES ('Entertainment', 'Easy', 10);
 
 INSERT INTO UserScore (user_id, quiz_id, score)
 VALUES (1,1,5);
