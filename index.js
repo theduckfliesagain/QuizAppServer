750137ef267c9b1bf306235323f4837cbbb2dad9
James Wheadon <james.wheadon@yahoo.com>
2021-04-26 17:21:52 +0100

    build object linking socket IDs and usernames

--- index.js
+++ index.js
@@ -11,22 +11,29 @@ const options = {
 };
 const io = socketio(server, options);
 
+let socketUsernames = {};
+
 io.on('connection', (socket) => {
     console.log('a user connected');
     socket.emit('admin-message', 'Hi there, new friend!')
     socket.broadcast.emit('admin-message', `A new friend has arrived!`)
 
     socket.on('disconnect', () => {
+        if (socket.id.values().next().value in socketUsernames) {
+            delete socketUsernames[socket.id.values().next().value];
+        }
         console.log('user disconnected');
     });
 
     socket.on('request-join-game', ({room, username}) => {
-
+        console.log(socket.id)
+        socketUsernames[socket.id.values().next().value] = username;
         socket.join(room)
         socket.broadcast.to(room).emit('add-user', { username })
 
         const roomData = io.sockets.adapter.rooms.get(room);
         const inRoomCount = roomData.size
+        console.log(roomData)
 
         socket.to(room).emit('new-player-joining', { username, room })
         io.in(room).emit('admin-message', `${inRoomCount} players now in ${room}!`)
