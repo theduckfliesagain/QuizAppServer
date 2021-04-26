5e7c889e49af8d80ad83967b0182f46fb93000e4
James Wheadon <james.wheadon@yahoo.com>
2021-04-26 10:31:41 +0100

    add route for sockets

--- server.js
+++ server.js
@@ -6,8 +6,10 @@ server.use(cors());
 server.use(express.json());
 
 const userRoutes = require('./routes/users')
+const socketRoutes = require('./routes/socket')
 
 server.use('/users', userRoutes)
+server.use('/socket', socketRoutes)
 
 // Root route
 server.get('/', (req, res) => res.send('Hello, world!'))
