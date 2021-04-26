986510172153bea67754b48b9bc2050018335eee
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-04-26 16:56:05 +0100

    add additional error info

--- models/User.js
+++ models/User.js
@@ -24,6 +24,7 @@ class User {
         return new Promise (async (resolve, reject) => {
             try {
                 const result = await db.query(SQL`SELECT * FROM users WHERE name = ${name};`)
+                if (result.rows.length === 0) throw new Error(`No user with the name ${name}.`)
                 const user = new User(result.rows[0]);
                 resolve(user);
             } catch (err) {
