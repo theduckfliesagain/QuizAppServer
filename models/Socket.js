21e97e0f2d44ebbeb859c0a2ad0c5ae1ac317e85
James Wheadon <james.wheadon@yahoo.com>
2021-04-26 14:29:43 +0100

    message between clients

--- models/Socket.js
+++ models/Socket.js
@@ -2,6 +2,8 @@ const socketio = require("socket.io");
 
 module.exports = (socket) => {
     console.log('a user connected');
+    socket.emit('admin-message', 'Hi there, new friend!')
+    socket.broadcast.emit('admin-message', `A new friend has arrived!`)
     socket.on('disconnect', () => {
         console.log('user disconnected');
     });
