1878fdfd83473f5d7f8054b821e84d0c62b014cb
James Wheadon <james.wheadon@yahoo.com>
2021-04-26 10:58:25 +0100

    add cors policy to socket server

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 const app = require('./server');
+const http = require("http")
+const socketio = require("socket.io");
+const server = http.createServer(app);
+const options = {
+    cors: {
+        origin: "http://localhost:8080/",
+        methods: ["GET", "POST"]
+    }
+};
+const io = socketio(server, options);
 
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`Express now departing from port ${port}!`))
+server.listen(port, () => console.log(`Express now departing from port ${port}!`))
--- routes/socket.js
+++ routes/socket.js
@@ -3,18 +3,14 @@ const router = express.Router();
 const io = require("socket.io")()
 
 //then in your router you can access it like this
-router.post('/getRides', function(req, res, next) {
+router.post('/getRides', function (req, res, next) {
 
     io.on('connection', (socket) => {
-      console.log('a user connected');
-      socket.on('disconnect', () => {
-        console.log('user disconnected');
-      });
+        console.log('a user connected');
+        socket.on('disconnect', () => {
+            console.log('user disconnected');
+        });
     });
-
-    db.rides.find(function(err, docs) {
-        res.json(docs);
-    })
 })
 
 module.exports = router;
