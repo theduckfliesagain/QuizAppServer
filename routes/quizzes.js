919024e16b484996a7239df022e7fa6ac6088dde
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-04-26 12:24:24 +0100

    add route for getting quiz players info

--- routes/quizzes.js
+++ routes/quizzes.js
@@ -3,9 +3,12 @@ const router = express.Router();
 const quizzesController = require('../controllers/quizzes')
 
 router.get('/', quizzesController.index)
-router.get('/:id', quizzesController.show)
 router.post('/', quizzesController.create)
+
+router.get('/:id', quizzesController.show)
 router.put('/:id', quizzesController.update)
+
+router.get('/:id/users', quizzesController.showUsers)
 // router.delete('/:id', quizzesController.destroy)
 
 module.exports = router;
