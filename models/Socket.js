c47c8b195b31a4686704b04846850ac52d7d3717
James Wheadon <james.wheadon@yahoo.com>
2021-04-26 15:02:03 +0100

    request-join-game method for websocket in server

--- models/Socket.js
+++ models/Socket.js
@@ -4,7 +4,23 @@ module.exports = (socket) => {
     console.log('a user connected');
     socket.emit('admin-message', 'Hi there, new friend!')
     socket.broadcast.emit('admin-message', `A new friend has arrived!`)
+
     socket.on('disconnect', () => {
         console.log('user disconnected');
     });
+
+    socket.on('request-join-game', ({gameId, username}) => {
+        console.log(`${username} joining ${gameId}`)
+
+        socket.join(gameId)
+        socket.broadcast.to(gameId).emit('add-user', { username })
+
+        const roomData = io.sockets.adapter.rooms[gameId]
+        const inRoomCount = roomData.length
+        const inRoomIds = Object.keys(roomData.sockets)
+
+        socket.emit('entry-permission', { gameId, players: inRoomIds})
+        socket.to(gameId).emit('new-player-joining', { username, gameId })
+        io.in(gameId).emit('admin-message', `${inRoomCount} players now in ${gameId}!`)
+    })
 };
