1746fe3e9ae84ccceaacc8b15fa84505bdbdefa4
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-04-27 11:15:31 +0100

    remove destroy function

--- controllers/quizzes.js
+++ controllers/quizzes.js
@@ -55,18 +55,9 @@ async function updateUserScore (req, res) {
         const userScores = await quiz.updateUserScore({ user, score: req.body.score});
         res.status(200).json(userScores)
     } catch (err) {
+        console.log(err);
         res.status(404).json({err})
     }
 }
 
-async function destroy (req, res) {
-    try {
-        const quiz = await Quiz.findById(req.params.id);
-        const resp = await quiz.destroy();
-        res.status(204).end();
-    } catch (err) {
-        res.status(404).json({err});
-    };
-}
-
-module.exports = { index, show, showUsers, create, update, updateUserScore, destroy }
+module.exports = { index, show, showUsers, create, update, updateUserScore }
