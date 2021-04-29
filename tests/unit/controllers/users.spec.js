b70372abd96ae8900b0018cbef3ac2edffddfc9d
James Wheadon <james.wheadon@yahoo.com>
2021-04-29 10:36:38 +0100

    increase users controller coverage

--- tests/unit/controllers/users.spec.js
+++ tests/unit/controllers/users.spec.js
@@ -52,4 +52,22 @@ describe('users controller', () => {
             expect(mockJson).toHaveBeenCalledWith(new User(testUser));
         })
     });
+
+    describe('update', () => {
+        test('it returns an updated user with a 200 status code', async () => {
+            let testUser = {
+                id: 1, name: 'Test User', highscore: 1
+            }
+            const resultUser = {
+                id: 1, name: 'Test User', highscore: 10}
+            jest.spyOn(User, 'findByName')
+                .mockResolvedValue({...new User(testUser), update: function(highscore) {
+                    return new User({...testUser, highscore: highscore})} });
+                
+            const mockReq = { params: {name: 'Test User' }, body: 10 }
+            await usersController.update(mockReq, mockRes);
+            expect(mockStatus).toHaveBeenCalledWith(200);
+            expect(mockJson).toHaveBeenCalledWith(new User(resultUser));
+        })
+    });
 })
